281a48fa75044aa516b9be1055876677b904d976
sanskar sahu <sahup3296@gmail.com>
2022-02-14 12:32:33 +0530

    initial commit

---
+++ dsmark/pages/index.jsx
@@ -0,0 +1,15 @@
+import { Header, Home, About, Service, Contact } from "../components";
+
+const index = () => {
+  return (
+    <>
+      <Header />
+      <Home />
+      <About />
+      <Service />
+      <Contact />
+    </>
+  );
+};
+
+export default index;
