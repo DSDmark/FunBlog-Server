d04061869c49aef934736e83cba68419afa42758
Sanskar Sahu <sahup3296@gmail.com>
2022-10-03 16:08:51 +0000

    adding config

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = 'James Doe';
+console.log(name); // 👉️ "James Doe"
--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,24 @@
-const name = 'James Doe';
+import dotenv from "dotenv";
+dotenv.config();
 
-console.log(name); // 👉️ "James Doe"
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
 
-export {};
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use(morgan("dev"));
+app.use(cookieParser());
+
+const PORT = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+	res.send({ msg: "Hey dude..." })
+})
+
+app.listen(PORT, () => {
+	console.log(`Server Running on ${PORT}`);
+})
