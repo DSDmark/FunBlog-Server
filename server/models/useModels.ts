52c22d362f7f09bdcb4ca566d23c9a3e833678cc
Sanskar Sahu <sahup3296@gmail.com>
2022-10-05 03:42:41 +0000

    API READY

---
+++ server/models/useModels.ts
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+interface IUser {
+	name: string;
+	email: string;
+	password: string;
+	avatar: string;
+	role: string;
+	type: string;
+	token: string;
+}
+
+const userSchema = new mongoose.Schema({
+	name: {
+		tyle: String,
+		required: [true, "Add your name"],
+		trim: true,
+		maxLength: [20, "Your name is too long"]
+	},
+	email: {
+		type: String,
+		required: [true, "Enter your email"],
+		trim: true,
+		unique: true
+	},
+	password: {
+		type: String,
+		required: [true, "Enter your password"],
+
+	},
+	avatar: {
+		type: String,
+		default: "https://res.cloudinary.com/devatchannel/image/upload/v1602752402/avatar/avatar_cugq40.png"
+	},
+	role: {
+		type: String,
+		default: "normal"
+	},
+	type: {
+		type: String,
+		default: "register"
+	},
+	token: { type: String, select: false }
+}, { timestamps: true });
+
+export default mongoose.model<IUser>("user", userSchema);
