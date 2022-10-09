7b737e2b1ea0acce105347a54ad80a2648795d31
Sanskar Sahu <sahup3296@gmail.com>
2022-10-09 15:51:25 +0000

    ADDED SMS_SENDER

---
+++ server/config/sendSms.ts
@@ -0,0 +1,20 @@
+import { Twilio } from "twilio";
+const smsNo = process.env['SMS_NO']
+const accountSid = process.env['SMS_SID']
+const authToken = process.env['SMS_TOKEN']
+const client = new Twilio(`${accountSid}`, `${authToken}`);
+
+export const sendSms = async (to: string, body: string, txt: string) => {
+	try {
+		client.messages
+			.create({
+				body: `blogServices ${body} - ${txt}`,
+				from: smsNo,
+				to,
+			})
+			.then(message => console.log(message.sid));
+
+	} catch (error) {
+		console.log(error);
+	}
+}
