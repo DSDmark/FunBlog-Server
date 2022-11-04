2668f897e0fdf57b19eb3ee620f32774f01b24c3
Sanskar Sahu <sahup3296@gmail.com>
2022-11-04 13:04:37 +0000

    PageNotFound was added

--- client/src/PageRender.tsx
+++ client/src/PageRender.tsx
@@ -1,15 +1,29 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { createElement } from "react";
+import { useParams, Params } from "react-router-dom";
+import PageNotFound from "./components/global/PageNotFound";
 
-const PageRender = ()=>{
-	const param = useParams();
-	console.log(param);
+const generatePage = (name: string) => {
+	const component = () => require(`./pages/${name}`).default;
+
+	try {
+		return createElement(component());
+	} catch (err) {
+		return <PageNotFound />;
+	}
+
+}
+
+const PageRender = () => {
+	const { page, slug }: Params = useParams();
+	let name: string = "";
+
+	if (page) name = slug ? `${page}/[slug]` : `${page}`;
+
 	return (
-		<>
 		<div>
-		pageRender:_
+			{generatePage(name)}
+
 		</div>
-		</>
 	)
 }
 
