94950a424d49abb2eb02d4ff90e0847e3511718d
Sanskar Sahu <sahup3296@gmail.com>
2022-11-18 07:32:47 +0000

    CLIENT WAS DELETED

--- client/src/components/index.tsx
+++
@@ -1,4 +0,0 @@
-export {default as Header} from "./global/Header";
-export {default as Search} from "./global/Search";
-export {default as PageNotFound} from "./global/PageNotFound";
-export {default as Footer} from "./global/Footer";
--- client/src/utils/TypeScript.tsx
+++
@@ -1,3 +0,0 @@
-import {ChangeEvent} from "react";
-
-export type InputChange = ChangeEvent<HTMLInputElement>
